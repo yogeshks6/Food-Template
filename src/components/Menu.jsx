c95150db8bd56f53c8a4051a92fe198034a4823d
Yogesh kuamr <yogeshkumarsoni66@gmail.com>
2024-06-18 14:23:00 +0530

    format

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,20 +1,20 @@
 export const Menu = () => {
     return (
         <>
-        <div className="row">
-            <div className="d-lg-flex menu align-items-center">
-                <div className="col-12 col-lg-5 col-md-5">
-                    <img src="images/menu-img-1.png" className="w-100" alt="" />
-                </div>
-                <div className="col-12 col-lg-7 col-md-7">
-                    <h1 className="menu-h1">Chicken</h1>
-                    <div className="d-flex justify-content-between">
-                        <h3>Finger Chicken</h3>
-                        <img src="images/menu-img-rate.png" className="w-25 h-25" alt="" />
-                        <h3 className="me-5">$12</h3>
+            <div className="row">
+                <div className="d-lg-flex menu align-items-center">
+                    <div className="col-12 col-lg-5 col-md-5">
+                        <img src="images/menu-img-1.png" className="w-100" alt="" />
+                    </div>
+                    <div className="col-12 col-lg-7 col-md-7">
+                        <h1 className="menu-h1">Chicken</h1>
+                        <div className="d-flex justify-content-between">
+                            <h3>Finger Chicken</h3>
+                            <img src="images/menu-img-rate.png" className="w-25 h-25" alt="" />
+                            <h3 className="me-5">$12</h3>
+                        </div>
                     </div>
                 </div>
-            </div>
             </div>
         </>
     )
