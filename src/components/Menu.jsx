7842755d5e727d13b04fb4dc11129183bed5db63
Yogesh kuamr <yogeshkumarsoni66@gmail.com>
2024-06-18 16:50:05 +0530

    menu second setion complete

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,16 +3,85 @@ export const Menu = () => {
         <>
             <div className="row">
                 <div className="d-lg-flex menu align-items-center">
-                    <div className="col-12 col-lg-5 col-md-5">
+                    <div className="col-12 col-lg-5 col-md-5 ps-5">
                         <img src="images/menu-img-1.png" className="w-100" alt="" />
                     </div>
-                    <div className="col-12 col-lg-7 col-md-7">
-                        <h1 className="menu-h1">Chicken</h1>
-                        <div className="d-flex justify-content-between">
-                            <h3>Finger Chicken</h3>
-                            <img src="images/menu-img-rate.png" className="w-25 h-25" alt="" />
-                            <h3 className="me-5">$12</h3>
+                    <div className="col-12 col-lg-7 col-md-7 ps-5">
+                        <h1 className="menu-h1 mb-4 fw-normal">Chicken</h1>
+                        <div className="d-flex mb-2">
+                            <h4 className="w-100 me-5 fw-normal">Finger Chicken</h4>
+                            <img src="images/menu-img-rate.png" className="w-25 h-25 ms-5" alt="" />
+                            <h4 className="me-5 w-100 text-end fw-normal">$12</h4>
                         </div>
+                        <div className="d-flex mb-2">
+                            <h4 className="w-100 me-5 fw-normal">Chicken Grilled</h4>
+                            <img src="images/menu-img-rate.png" className="w-25 h-25 ms-5" alt="" />
+                            <h4 className="me-5 w-100 text-end fw-normal">$13</h4>
+                        </div>
+                        <div className="d-flex mb-2">
+                            <h4 className="w-100 me-5 fw-normal">Chicken Prame</h4>
+                            <img src="images/menu-img-rate.png" className="w-25 h-25 ms-5" alt="" />
+                            <h4 className="me-5 w-100 text-end fw-normal">$11</h4>
+                        </div>
+                        <div className="d-flex mb-2">
+                            <h4 className="w-100 me-5 fw-normal">Chicken Wrap</h4>
+                            <img src="images/menu-img-rate.png" className="w-25 h-25 ms-5" alt="" />
+                            <h4 className="me-5 w-100 text-end fw-normal">$8</h4>
+                        </div>
+                        <div className="d-flex mb-2">
+                            <h4 className="w-100 me-5 fw-normal">Chicken Marsala</h4>
+                            <img src="images/menu-img-rate.png" className="w-25 h-25 ms-5" alt="" />
+                            <h4 className="me-5 w-100 text-end fw-normal">$14</h4>
+                        </div>
+                        <div className="d-flex mb-2">
+                            <h4 className="w-100 me-5 fw-normal">Chicken Chilli</h4>
+                            <img src="images/menu-img-rate.png" className="w-25 h-25 ms-5" alt="" />
+                            <h4 className="me-5 w-100 text-end fw-normal">$12</h4>
+                        </div>
+                      
+                    </div>
+                </div>
+
+
+
+
+
+                <div className="d-lg-flex  mt-5 align-items-center">
+                    <div className="col-12 col-lg-7 col-md-7 ps-5">
+                        <h1 className="menu-h1 mb-4 fw-normal">Breakfast</h1>
+                        <div className="d-flex mb-2">
+                            <h4 className="w-100 me-5 fw-normal">Berries</h4>
+                            <img src="images/menu-img-rate.png" className="w-25 h-25 ms-5" alt="" />
+                            <h4 className="me-5 w-100 text-end fw-normal">$12</h4>
+                        </div>
+                        <div className="d-flex mb-2">
+                            <h4 className="w-100 me-5 fw-normal">Cold Cereal</h4>
+                            <img src="images/menu-img-rate.png" className="w-25 h-25 ms-5" alt="" />
+                            <h4 className="me-5 w-100 text-end fw-normal">$13</h4>
+                        </div>
+                        <div className="d-flex mb-2">
+                            <h4 className="w-100 me-5 fw-normal">Cottage Cheese</h4>
+                            <img src="images/menu-img-rate.png" className="w-25 h-25 ms-5" alt="" />
+                            <h4 className="me-5 w-100 text-end fw-normal">$12</h4>
+                        </div>
+                        <div className="d-flex mb-2">
+                            <h4 className="w-100 me-5 fw-normal">Eggs</h4>
+                            <img src="images/menu-img-rate.png" className="w-25 h-25 ms-5" alt="" />
+                            <h4 className="me-5 w-100 text-end fw-normal">$8</h4>
+                        </div>
+                        <div className="d-flex mb-2">
+                            <h4 className="w-100 me-5 fw-normal">Green Tea</h4>
+                            <img src="images/menu-img-rate.png" className="w-25 h-25 ms-5" alt="" />
+                            <h4 className="me-5 w-100 text-end fw-normal">$14</h4>
+                        </div>
+                        <div className="d-flex mb-2">
+                            <h4 className="w-100 me-5 fw-normal">Peanut Butter</h4>
+                            <img src="images/menu-img-rate.png" className="w-25 h-25 ms-5" alt="" />
+                            <h4 className="me-5 w-100 text-end fw-normal">$22</h4>
+                        </div>
+                    </div>
+                    <div className="col-12 col-lg-5 col-md-5 pe-5">
+                        <img src="images/menu-img-2.png" className="w-100" alt="" />
                     </div>
                 </div>
             </div>
