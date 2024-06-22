920f632be437c02045823564257dfb9bb4a905f5
Yogesh kuamr <yogeshkumarsoni66@gmail.com>
2024-06-22 13:44:20 +0530

    some add

--- src/components/FoodCard.jsx
+++ src/components/FoodCard.jsx
@@ -16,20 +16,20 @@ export const FoodCard = () => {
                     <div className="col-12 col-lg-12 col-sm-12 m-5">
                         <div className="card-food rounded-5  pb-2">
                             <div className="card-food-heart float-end p-3 px-4">
-                                <i className="fa-regular fa-heart" onClick={"document.getElementById('myform').style.color = 'white';"}></i>
-                            </div>
-                            <img src={currElem.prroduct_img} className=" card-food-img pt-2 pb-2" alt="" />
-                            <div className="ps-4">
-                                <h2>{currElem.product_name}</h2>
-                                <p>{currElem.description}</p>
-                                <p className="d-flex justify-content-between">
-                                    <span><i className="fa-solid fa-dollar-sign"></i>{currElem.price}</span>
-                                    <span className="pe-4"><i className="fa-solid fa-star"></i>{currElem.rating}</span>
-                                </p>
-                            </div>
+                                <i className="fa-regular fa-heart" id="white"></i>
                         </div>
-                    </div>)
-            })}
+                        <img src={currElem.prroduct_img} className=" card-food-img pt-2 pb-2" alt="" />
+                        <div className="ps-4">
+                            <h2>{currElem.product_name}</h2>
+                            <p>{currElem.description}</p>
+                            <p className="d-flex justify-content-between">
+                                <span><i className="fa-solid fa-dollar-sign"></i>{currElem.price}</span>
+                                <span className="pe-4"><i className="fa-solid fa-star"></i>{currElem.rating}</span>
+                            </p>
+                        </div>
+                    </div>
+                    </div >)
+})}
 
         </>
     )
